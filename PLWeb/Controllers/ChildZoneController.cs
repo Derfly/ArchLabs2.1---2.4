0973e7a5df6a3191d27d5544e45484e5ebb24c29
Derfly <65560079+Derfly@users.noreply.github.com>
2020-05-31 08:52:28 +0300

    Add files via upload

---
+++ PLWeb/Controllers/ChildZoneController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UserInterface;
+
+namespace PLWeb.Controllers
+{
+    public class ChildZoneController : ApiController
+    {
+        // GET: api/ChildZone
+        /*public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        } */
+
+        // GET: api/ChildZone/5
+        public string Get(int id)
+        {
+            //Program Main = new Program();
+            //Main.Main();
+            return "You chosed first UI element";
+        }
+
+        // POST: api/ChildZone
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ChildZone/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ChildZone/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PLWeb/Controllers/UIController.cs
@@ -0,0 +1,56 @@
+﻿using LR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UserInterface;
+using BLL.Entities;
+
+namespace PLWeb.Controllers
+{
+    public class UIController : ApiController
+    {
+        // GET: api/UI
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/UI/5
+        public ChildZone Get(int id)
+        {
+            UI_API api = new UI_API();
+            try
+            {
+                return api.FindZone(id);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        // POST: api/UI
+        public void Post([FromBody]ChildZone value)
+        {
+            UI_API api = new UI_API();
+            api.CreateNewChildZone(value);
+        }
+
+        // PUT: api/UI/5
+        public void Put(int id, [FromBody]ChildZone value)
+        {
+            UI_API api = new UI_API();
+            api.PutChildZone(id, value);
+        }
+
+        // DELETE: api/UI/5
+        public void Delete(int id)
+        {
+            UI_API api = new UI_API();
+            api.DeleteChildZone(id);
+        }
+    }
+}
